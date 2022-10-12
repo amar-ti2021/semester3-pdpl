1f803520b7c885162798e7b5e8260f4c89c5df9b
amar-ti2021 <muha21095ti@student.nurulfikri.ac.id>
2022-10-12 08:56:03 +0700

    Add pertemuan1/Tugas1.java

---
+++ pertemuan1/Tugas1.java
@@ -0,0 +1,8 @@
+public class Tugas1 {
+    public static void main(String[] args){
+        System.out.println("===========================");
+        System.out.println("Nama  : Muhammad Amar Dafi");
+        System.out.println("Kelas : 3TI04");
+        System.out.println("NIM   : 0110221095");
+    }
+}
